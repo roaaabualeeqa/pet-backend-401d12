d2eb88156e4bf19f3962bebfa37d9b0b34c2f6be
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2024-08-27 08:46:27 +0000

    remove docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3
+# FROM python:3
 
-ENV PYTHONDONTWRITEBYTECODE 1
+# ENV PYTHONDONTWRITEBYTECODE 1
 
-ENV PYTHONUNBUFFERED 1
+# ENV PYTHONUNBUFFERED 1
 
-RUN mkdir /code
-WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
-COPY . /code/
+# RUN mkdir /code
+# WORKDIR /code
+# COPY requirements.txt /code/
+# RUN pip install -r requirements.txt
+# COPY . /code/
--- pets_project/settings.py
+++ pets_project/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'pets_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# connect django to postgres 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# connect django to postgres 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 # connect django to postgres 
 # DATABASES = {
